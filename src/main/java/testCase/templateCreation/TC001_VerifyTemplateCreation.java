3114932c2b0e282c9d7c19152b7988e8481977d6
raghav.k911 <raghav.k911@gmail.com>
2023-10-19 09:58:44 +0530

    updated

--- src/main/java/testCase/templateCreation/TC001_VerifyTemplateCreation.java
+++ src/main/java/testCase/templateCreation/TC001_VerifyTemplateCreation.java
@@ -3,7 +3,6 @@ package testCase.templateCreation;
 import java.util.concurrent.TimeUnit;
 
 import org.testng.Reporter;
-
 import org.testng.annotations.Test;
 
 import util.templateCreation.BaseClass;
@@ -12,44 +11,53 @@ import webElement.templateCreation.SalesforceWebElement;
 public class TC001_VerifyTemplateCreation {
 	SalesforceWebElement tempCreate;
 	@Test
-	public void tC001_VerifyTemplateCreation(){
+	public void tC001_VerifyTemplateCreation() throws InterruptedException{
 		try {
 			BaseClass.launchBrowser("SalesforceDriver");
 			tempCreate = new SalesforceWebElement();
-			tempCreate.isNewButtonClicked();
-			
+
 			BaseClass.driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+			tempCreate.isNewButtonClicked();
+
+			Thread.sleep(5000);
 			tempCreate.isNameTextBoxClicked();
 			tempCreate.enterNameText();
-			
+
+
 			BaseClass.driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 			tempCreate.isDescriptionTextBoxClicked();
 			tempCreate.enterDescriptionText();
-			
+
 			BaseClass.driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 			tempCreate.isTargetObjTextBoxClicked();
 			tempCreate.enterTargetObjectText();
-			
-			//BaseClass.driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
 			tempCreate.isCombobxValueSelected();
-			
-			//BaseClass.driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
 			tempCreate.isNextNewTempBtnClicked();
-			
-			//BaseClass.driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
 			tempCreate.isWhatsAppChannelAdded();
-			
-			//BaseClass.driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
 			tempCreate.isTempleteTypeClicked();
 			tempCreate.selectTemplateType();
-			
-			//BaseClass.driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
 			tempCreate.isAttributeNameFieldClicked();
 			tempCreate.selectAttributeName();
+			tempCreate.isInsertAttributeButtonClicked();
 
-		} catch (InterruptedException e) {
-			Reporter.log("Browser is not launched ::"+e.getMessage());
-			e.printStackTrace();
+			tempCreate.isNextWATemplateBtnClicked();
+			tempCreate.isTextRadioButtonClicked();
+			tempCreate.enterTextWATemplate();
+			tempCreate.enterFooterText();
+			tempCreate.isNextWAHeaderButtonClicked();
+
+			tempCreate.isWAIntTempRadioButtonClicked();
+			tempCreate.selectLabelTextField();
+			tempCreate.enterTextInLabelTextField();
+			tempCreate.isFinishButtonClicked();
+		}catch(Exception e) {
+			Reporter.log("Chrome is not launched"+e.getMessage());
+			e.getStackTrace();
 		}
 	}
 }
